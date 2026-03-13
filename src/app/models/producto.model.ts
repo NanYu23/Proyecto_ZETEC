730232e100383d13d98df4cbcd9f164f3a770520
Itzel <glitzel11@gmail.com>
2026-03-13 09:22:27 -0600

    Creación de archivos necesarios

---
+++ src/app/models/producto.model.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: number; //declarar variables
+    name: string;
+    price: number;
+    imageUrl: string;
+    category: string;
+    description: string;
+    inStock: boolean;
+}
